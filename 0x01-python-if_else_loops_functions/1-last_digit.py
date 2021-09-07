9351327845921740f022529ca68b7004bf3233b8
enzonjagi <amonsdungo@gmail.com>
2021-09-08 01:36:49 +0300

    Last digit trial 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if (number < 0):
-    x = -(number) % 10
-elif number == 0:
-    x = number
+# find last digit
+if number == 0:
+    x = 0
+elif number < 0:
+    x = (-number) % 10
 else:
     x = number % 10
+# print message based on the last digit
 if x > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, x))
-elif (x != 0) and (x < 6):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(
-                    number, x))
 elif x == 0:
     print("Last digit of {} is {} and is 0".format(number, x))
+elif (x > 0) and (x < 6):
+    print(
+        "Last digit of {} is {} and is less than 6 and not 0".format(
+            number, x))
