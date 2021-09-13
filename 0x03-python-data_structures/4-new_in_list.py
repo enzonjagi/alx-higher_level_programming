2f226d8dddd9b807431a41196848aa941785dc43
enzonjagi <amonsdungo@gmail.com>
2021-09-14 00:52:51 +0300

    Replace in a copy: seems the fix was obvious, maybe?

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    # rethink
     length = len(my_list)
-    lsCopy = []
-    lsCopy = my_list[:]
-    # check idx value
-    if (idx < 0) or (idx > length):
-        return lsCopy
-    else:
-        my_list[idx] = element
+    copyL = my_list[:]
+    if (idx < 0) or (idx > (length)):
         return my_list
+    else:
+        copyL[idx] = element
+        return copyL
