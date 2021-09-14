55dff7c996665339650525d9bfbfc8ec586e2459
enzonjagi <amonsdungo@gmail.com>
2021-09-14 14:58:13 +0300

    Print Reverse: Bug was, excluding None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    my_list = my_list if my_list and type(my_list) is list else []
+    if my_list is not None:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
