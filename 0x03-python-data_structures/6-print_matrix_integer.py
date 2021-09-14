4b79e1a4d9135c41bf72d0745979a4c2ccea52cb
enzonjagi <amonsdungo@gmail.com>
2021-09-14 03:24:08 +0300

    Print a matrix: row by row, took time though!

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{}".format(matrix[i][j]), end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
             print()
