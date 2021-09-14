1b718b5a3e78a7edfe38ffb6d797505df5f1d4f1
enzonjagi <amonsdungo@gmail.com>
2021-09-14 15:17:52 +0300

    4 debug: >= missing

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     # rethink
     length = len(my_list)
     copyL = my_list[:]
-    if (idx < 0) or (idx > (length)):
+    if (idx < 0) or (idx >= (length)):
         return my_list
     else:
         copyL[idx] = element
