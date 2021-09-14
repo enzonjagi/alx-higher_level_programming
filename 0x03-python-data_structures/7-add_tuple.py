fa20747e238f5de36919707b17e8b2c49a0d2fe8
enzonjagi <amonsdungo@gmail.com>
2021-09-14 16:24:00 +0300

    7 debug: check if len == 1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    item1 = 0
-    item2 = 0
-    if (len(tuple_a) >= 2) and (len(tuple_b) >= 2):
-        item1, item2 = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
-    elif (len(tuple_a) < 2):
-        if (len(tuple_a) == 0):
-            item1, item2 = (0 + tuple_b[0]), (0 + tuple_b[1])
-        else:
-            item1, item2 = (tuple_a[0] + tuple_b[0]), (0 + tuple_b[1])
-    elif (len(tuple_b) < 2):
-        if len(tuple_b) == 0:
-            item1, item2 = (tuple_a[0] + 0), (tuple_a[1] + 0)
-        else:
-            item1, item2 = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0)
-    else:
-        item1, item2 = 0, 0
-    tuple_c = item1, item2
-    return tuple_c
+    a, b, c, d = 0, 0, 0, 0
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            a = tuple_a[0]
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            b = tuple_b[0]
+    if len(tuple_a) >= 2:
+        a, c = tuple_a[0], tuple_a[1]
+    if len(tuple_b) >= 2:
+        b, d = tuple_b[0], tuple_b[1]
+    add_t = a + b, c + d
+    return add_t
