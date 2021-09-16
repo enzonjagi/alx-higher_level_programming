7338a03ad91afc8b4ce55bb7a46e189868706115
enzonjagi <amonsdungo@gmail.com>
2021-09-16 03:58:58 +0300

    Simple square matrix: Took a while!

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squares = list(map(list, matrix))
-    for i in range(len(squares)):
-        for j in range(len(squares)):
-            squares[i][j] **= 2
+    if matrix is not None:
+        squares = list(map(list, matrix))
+        for i in range(len(squares)):
+            for j in range(len(squares)):
+                squares[i][j]**=2
     return squares
