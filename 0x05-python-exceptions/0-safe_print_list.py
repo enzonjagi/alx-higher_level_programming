93d06c51f111f4df6c3c1cdbce535e76b2cae0d7
enzonjagi <amonsdungo@gmail.com>
2021-09-28 04:47:52 +0300

    README and Safe Print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/python3/bin
+def safe_print_list(my_list=[], x=0):
+    b = x
+    c = 0
+    try:
+        for i in my_list:
+            if (i <= b):
+                print("{}".format(i), end="")
+            c += 1
+        print("")
+    except IndexError:
+        print("")
+    finally:
+        if (b > c):
+            return (c)
+        return (b)
