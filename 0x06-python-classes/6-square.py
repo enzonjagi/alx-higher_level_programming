8bf3c2287fbed02ffdee8c9335dc71069dc0d1eb
enzonjagi <amonsdungo@gmail.com>
2021-09-29 23:17:22 +0300

    Print a positioned square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+'''Square module with size check'''
+
+
+class Square:
+    '''An empty Class'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''A initialization function'''
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        '''Returns Area of square'''
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        '''Prints a literal square with hashes'''
+        if self.size <= 0:
+            print("")
+            return
+
+        [print("")for i in range(0, self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print("")
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    my_square_1.my_print()
+
+    print("--")
+
+    my_square_2 = Square(3, (1, 1))
+    my_square_2.my_print()
+
+    print("--")
+
+    my_square_3 = Square(3, (3, 0))
+    my_square_3.my_print()
+
+    print("--")
