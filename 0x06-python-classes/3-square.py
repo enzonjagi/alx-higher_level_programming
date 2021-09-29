fc0b482c0d12e4bf150a7c9f52c56b74d15a1654
enzonjagi <amonsdungo@gmail.com>
2021-09-29 22:15:27 +0300

    Finding area of a Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Square module with size check'''
+
+
+class Square:
+    '''An empty Class'''
+
+    def __init__(self, size=0):
+        '''A initialization function'''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
+
+    def area(self):
+        '''Returns Area of square'''
+        return(self.__size * self.__size)
