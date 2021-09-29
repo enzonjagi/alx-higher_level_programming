04d18e2354b699c6ef7143db22cad05209f46102
enzonjagi <amonsdungo@gmail.com>
2021-09-29 23:37:06 +0300

    Debug: list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,6 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
+            result = 0
             print("out of range")
         except TypeError:
             result = 0
