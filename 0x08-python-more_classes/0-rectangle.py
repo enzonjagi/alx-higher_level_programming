2f43a6d4fec022a701d8231134bcfe9e8d7e5ed7
enzonjagi <amonsdungo@gmail.com>
2021-10-04 21:42:35 +0300

    README and Execute 0-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The Rectangle module
+At this point, we're only providing
+an empty class
+"""
+
+
+class Rectangle:
+    """
+    The Rectangle class
+    Let's rock it
+    """
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,6 @@
+## 0x08 Python More Classes
+Class attributes
+<br/>Instance attributes
+<br/>Class methods
+<br/>static methods
+<br/>repr and str methods.
