ce619ebca09da8c03d3a681f0ab82a03a984f396
enzonjagi <amonsdungo@gmail.com>
2021-10-05 04:37:50 +0300

    debug attempt no. 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,16 +7,12 @@ Just a module; check class for more info
 
 class Rectangle:
     """
-    Need to set and get the width and height
-    Check for integer and raise TypeError if none exists
-    Checks if values are less than 0
+    A class in python3 that is
+    Inclusive of the area and perimeter
     """
 
     def __init__(self, width=0, height=0):
-        """
-        Instantiate rectangle with height and width
-        """
-
+        """Instantiate"""
         self.width = width
         self.height = height
 
@@ -28,7 +24,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
             self.__width__ = value
@@ -41,7 +37,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
             self.__height__ = value
