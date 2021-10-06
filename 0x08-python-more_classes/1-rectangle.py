3c1169b0ce80e4e6888694f8789949820a35b6e9
enzonjagi <amonsdungo@gmail.com>
2021-10-06 15:24:32 +0300

    Fixed a certain typo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return (self.__width__)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -27,11 +27,11 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("width must be >= 0")
         else:
-            self.__width__ = value
+            self.__width = value
 
     @property
     def height(self):
-        return (self.__height__)
+        return (self.__height)
 
     @height.setter
     def height(self, value):
@@ -40,7 +40,7 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("height must be >= 0")
         else:
-            self.__height__ = value
+            self.__height = value
 
 
 if __name__ == '__main__':
