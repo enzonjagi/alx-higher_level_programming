0303b0e6116838fb1c61930baa1b3128edd2208c
enzonjagi <amonsdungo@gmail.com>
2021-10-11 21:31:28 +0300

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#/usr/bin/python3
+"""
+module to check if object is an instance of a class
+or its subclass
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Implementation of the isinstance()
+    return: True or False
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
