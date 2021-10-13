a49706f51fd78fe0c41271c195707aba861ed9e7
enzonjagi <amonsdungo@gmail.com>
2021-10-13 12:56:50 +0300

    Script access JSON via a list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains:
+a script that adds all arguments to a Python list,
+and then save them to a file:
+"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+empty_list = []
+output_list = []
+# Add the arguments to the list
+for i in range(1, len(sys.argv)):
+    empty_list.append(sys.argv[i])
+# checking if the list is populated
+# print(empty_list)
+
+# Saving to JSON File
+my_file = "add_item.json"
+save_to_json_file(empty_list, my_file)
+
+# Load from JSON file
+output_list = load_from_json_file(my_file)
