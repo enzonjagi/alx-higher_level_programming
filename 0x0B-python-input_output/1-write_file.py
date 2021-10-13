5f7e745b0cfbff13ba2c6c338a20e8d9a290009d
enzonjagi <amonsdungo@gmail.com>
2021-10-13 04:58:19 +0300

    Overwriting a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a function that
+Opens a text file,
+Overwrites it
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Executing the purpose of this module:
+    Write to a text file(overwrite)
+    """
+
+    with open(filename, mode="w", encoding="utf-8")as f:
+        f.write(text)
