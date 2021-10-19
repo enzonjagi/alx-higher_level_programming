0714af6093b74199f6a1eb2893c497d012215edd
enzonjagi <amonsdungo@gmail.com>
2021-10-19 12:33:04 +0300

    Return a dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,9 +7,6 @@ for JSON serialization of an object
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     a function that returns the dictionary description
@@ -17,58 +14,4 @@ def class_to_json(obj):
     for JSON serialization of an object
     """
 
-    # descr = json.dumps(obj)
     return obj.__dict__
-
-
-if __name__ == '__main__':
-    class MyClass:
-        """ My class
-        """
-
-        def __init__(self, name):
-            self.name = name
-            self.number = 0
-
-        def __str__(self):
-            return "[MyClass] {} - {:d}".format(self.name, self.number)
-
-    m = MyClass("John")
-    m.number = 89
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
-
-    # Example 2
-    class MyClass:
-        """ My class
-        """
-
-        score = 0
-
-        def __init__(self, name, number = 4):
-            self.__name = name
-            self.number = number
-            self.is_team_red = (self.number % 2) == 0
-
-        def win(self):
-            self.score += 1
-
-        def lose(self):
-            self.score -= 1
-
-        def __str__(self):
-            return "[MyClass] {} - {:d} => {:d}".format(self.__name, self.number, self.score)
-
-    print("--------------------------")
-    n = MyClass("John")
-    n.win()
-    print(type(n))
-    print(n)
-
-    nj = class_to_json(n)
-    print(type(nj))
-    print(nj)
