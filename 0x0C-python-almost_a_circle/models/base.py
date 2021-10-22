5e7d0de1ac60826235dcf06fccd104fa97f6a298
enzonjagi <amonsdungo@gmail.com>
2021-10-22 11:01:32 +0300

    JSON rep

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,9 @@ The goal is to manage the id attribute and DRY
 '''
 
 
+import json
+
+
 class Base:
     '''
     the "base" class of all classes
@@ -23,3 +26,23 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+
+if __name__ == '__main__':
+    from base import Base
+    from rectangle import Rectangle
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
