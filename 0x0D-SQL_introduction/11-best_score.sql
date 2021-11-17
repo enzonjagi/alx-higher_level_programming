ce20a2a2ac70dfd005adf314a6a6fe2f52ba66f5
enzonjagi <amosndungo@gmail.com>
2021-11-17 02:05:00 -0800

    Select values with score >=10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Select score and name from second_table
+-- where score > 10
+-- order by score descending
+SELECT score, name FROM second_table
+       WHERE score>9
+       ORDER BY score DESC;
