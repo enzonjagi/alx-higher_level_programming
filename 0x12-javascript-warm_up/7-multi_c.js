7eb8eb65fae224bcaecae3511cdd139a916c199b
enzonjagi <amosndungo@gmail.com>
2021-12-14 09:01:53 +0300

    Print a statement x number of times; x being provided as a CLI argument

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv.slice(2);
+
+const count = parseInt(args[0]);
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
