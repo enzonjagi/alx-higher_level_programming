7c906c8f6667162e0e5aaee9cfe7dd37191793b6
enzonjagi <amosndungo@gmail.com>
2021-12-14 08:50:36 +0300

    Converting a string to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const args = process.argv.slice(2);
+
+const intValue = parseInt(args[0]);
+
+if (isNaN(intValue)) {
+  console.log('Not a number');
+} else {
+  console.log(intValue);
+}
