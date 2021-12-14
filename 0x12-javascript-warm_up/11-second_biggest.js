c1bfb4673a9c482d7ebaf8faf39f98c948f6c98c
enzonjagi <amosndungo@gmail.com>
2021-12-14 13:21:45 +0300

    Finding the second largest integer - debug: convert to int and using an array to check

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,19 +6,20 @@ const args = process.argv.slice(2);
 let largest = 0;
 let secondLargest = 0;
 
+const input = [];
 if ((args[0] === undefined) || (args.length === 1)) {
   console.log(0);
 } else {
   // find largest value
   for (let i = 0; i < args.length; i++) {
-    if (args[i] > largest) {
-      largest = args[i];
-    }
+    input.push(parseInt(args[i]));
   }
+  largest = Math.max(...input);
+
   // find second largest value
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] > secondLargest && args[i] < largest) {
-      secondLargest = args[i];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > secondLargest && input[i] < largest) {
+      secondLargest = input[i];
     }
   }
   console.log(secondLargest);
