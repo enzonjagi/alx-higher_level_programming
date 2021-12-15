78fc9447034ecace2d032ac44b8460534e17a651
enzonjagi <amosndungo@gmail.com>
2021-12-15 22:57:59 +0300

    Print out a square/rectangle with C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const ogsquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends ogsquare {
   charPrint (c) {
     let input = c;
     if (!c) {
