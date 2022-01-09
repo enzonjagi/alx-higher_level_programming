38d212a3ef3d853d105a959c42fb1a30acc09f26
enzonjagi <amosndungo@gmail.com>
2022-01-10 00:43:38 +0300

    Unnecessary files included

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,10 +31,48 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the JSON representation of list_dictionaries"""
         if list_dictionaries is None:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+            list_dictionaries = "[]"
+        return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns a list
+        The list contains the string repr of json_string
+        """
+        # checks if the json string is empty or None
+        # then returns an empty list
+        if json_string is None or len(json_string) == 0:
+            return []
+        # else it'll just return the string repr of
+        # json_string
+        return json.loads(json_string)
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of
+        list_objs to a file
+        """
+        filename = "{}.json".format(cls.__name__)
+        empty = []
+        if list_objs is not None:
+            for i in list_objs:
+                empty.append(cls.to_dictionary(i))
+        # calling the to_json method and
+        # writing the JSON to a file
+        with open(filename, mode='w') as f:
+            f.write(cls.to_json_string(empty))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """that returns an instance with all attributes already set:
+        **dictionary can be thought of as a double pointer to a dictionary
+        """
+        if cls.__name__ == "Rectangle":
+            mk = cls(1,1)
+        elif cls.__name__ == "Square":
+            mk = cls(1)
+        mk.update(**dictionary)
+        return mk
 
 if __name__ == '__main__':
     from base import Base
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
-'''
-Unittests for the base module,
-it should cover the following:
-1. 
-'''
+"""
+Unittests for the base module
+"""
+
+
+import unittest
+import pep8
+import json
+import inspect
+from models.base import Base
+
+class TestBaseDocumentation(unittest.TestCase):
+    """
+    Tests intended at checking:
+    1. Documentation of Base Class
+    2. Styling of the Base Class
+    """
+
+class TestBase(unittest.TestCase):
+    """
+    Tests intended at checking the following:
+    1. The Base class instantiation
+    2. Access of all the attributes and methods
+    """
