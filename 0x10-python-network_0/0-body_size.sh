37f0bb81eb4becfd848457a02c8a25504d03b4d4
enzonjagi <amosndungo@gmail.com>
2022-01-10 07:41:38 +0300

    Debug operation 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # accepts a URL, sends a request to the URL and returns size of response body
-curl "$1" -so /dev/null '%{size_download}\n'
+curl "$1" -sI | awk '/Content-Length/ { print $2 }'
