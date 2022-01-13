d09d137e29aaae7d3134a0a825f64b61896356a8
enzonjagi <amosndungo@gmail.com>
2022-01-13 23:30:50 +0300

    Debug POST email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
 
     data = {'email': sys.argv[2]}
 
-    r = requests.post(sys.argv[1], sys.argv[2])
-    print(r.content())
+    r = requests.post(sys.argv[1], email=sys.argv[2])
+    print(r.text())
