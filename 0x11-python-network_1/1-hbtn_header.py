fbd542d7ad07686eba33e5734d4987656bf2b172
enzonjagi <amosndungo@gmail.com>
2022-01-13 16:22:50 +0300

    1-hbtn_header.py: send request and get the value of the X-Request-Id header variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """Python script that displays value of X-Request-Id header variable"""
-
 import urllib.request
 import sys
 
 
 with urllib.request.urlopen(sys.argv[1]) as resp:
     '''store response in a variable'''
-    r = resp.info()
-
-print(r.get('X-Request-Id'))
+    print(resp.info().get('X-Request-Id'))
