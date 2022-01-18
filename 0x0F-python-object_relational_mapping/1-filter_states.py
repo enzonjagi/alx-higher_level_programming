dd91cb79c9f73c8cd21a4bf0a2c24fef0e76d339
enzonjagi <amosndungo@gmail.com>
2022-01-18 07:55:58 +0300

    Filter states table by names starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Getting all values from database.table
+where name starts with N
+order_by table.id
+'''
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=passwd,
+                         db=database)
+    cur = db.cursor()
+    cur.execute(
+        'SELECT * FROM states WHERE name regexp "^N.*" ORDER BY id')
+    result = cur.fetchall()
+
+    for x in result:
+        print(x)
