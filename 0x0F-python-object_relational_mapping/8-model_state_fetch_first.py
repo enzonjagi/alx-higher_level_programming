10976f3101fe504c82f70a780ee5b7e2194092ef
enzonjagi <amosndungo@gmail.com>
2022-01-18 16:26:01 +0300

    fetch the first value from the states table in the db

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Script that lists all State objects from hbtn_0e_6_usa'''
+
+
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    # check if arguments exist then work, if not, do nothing
+    if sys.argv[3] is not None:
+        uname = sys.argv[1]
+        pwd = sys.argv[2]
+        db = sys.argv[3]
+
+        # create the connection to the database
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(uname, pwd, db),
+            connect_args=dict(host="localhost", port=3306)
+        )
+        Base.metadata.create_all(engine)
+
+        # create a session and bind it to engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Query db.table to get the states details
+        for instance in session.query(State).order_by(State.id)[0:1]:
+            if instance is not None:
+                print('{}: {}'.format(instance.id, instance.name))
