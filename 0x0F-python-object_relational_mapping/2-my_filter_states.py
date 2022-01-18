0b50cedc9b7a01ec9e7effbc662f2aae2bebec89
enzonjagi <amosndungo@gmail.com>
2022-01-18 08:06:04 +0300

    a script that displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Getting all values from database.table
+where name provided as user input
+order_by table.id
+'''
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    filter_by = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=passwd,
+                         db=database)
+    cur = db.cursor()
+    cur.execute(
+        'SELECT * FROM states WHERE name = %s ORDER BY id', [filter_by])
+    result = cur.fetchall()
+
+    cur.close()
+    db.close()
+    for x in result:
+        print(x)
