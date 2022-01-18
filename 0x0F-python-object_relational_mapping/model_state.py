b9fdf813fc426c046ed45bbeeb3bdb20a758244b
enzonjagi <amosndungo@gmail.com>
2022-01-18 11:55:43 +0300

    Creating a state model using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,12 +13,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    '''State model
-    Contains the definition of the State Table
-    as seen below
-    '''
+    '''State class Contains the definition of the State Table'''
     __tablename__ = 'states'
     id = Column(
-        Integer, primary_key=True, nullable=False)
+        Integer, primary_key=True, nullable=False,
+        autoincrement=True, unique=True)
     name = Column(
         String(128), nullable=False)
