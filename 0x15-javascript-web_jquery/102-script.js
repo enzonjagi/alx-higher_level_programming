8dfce47f0278e24521f523f764ef0a5e900fe666
enzonjagi <amosndungo@gmail.com>
2022-02-10 17:32:12 +0300

    Hello, Salut, Hola - Translations

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+let thas = '';
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      thas = data.hello;
+      $('DIV#hello').html(thas);
+    });
+  });
+});
