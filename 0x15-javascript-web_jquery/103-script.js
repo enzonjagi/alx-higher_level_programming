4059d9dce27c28ac2878bbc39194b60913ccae42
enzonjagi <amosndungo@gmail.com>
2022-02-10 17:37:06 +0300

    Translations - now can use the enter key to perform translation

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,20 @@
+const $ = window.$;
+let thas = '';
+$('document').ready(function () {
+  $('INPUT#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
+      if (e.keyCode === 13) {
+        translate();
+      }
+    });
+  });
+});
+
+function translate () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+    thas = data.hello;
+    $('DIV#hello').html(thas);
+  });
+}
